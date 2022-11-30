0626404a4ce94a069b59823a236de552d8692d5b
Luis Gustavo <70583538+Lu97Sierra@users.noreply.github.com>
2022-11-30 13:54:00 -0600

    origin

---
+++ CRUD API REST/router_doc/servidor.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const {query}= require('express')
+const cors = require('cors')
+const ruta_consejo = require('./final.js')
+const path=require('path')
+
+
+const swaggerUI     = require('swagger-ui-express');
+const swaggerJsDoc  = require('swagger-jsdoc');
+
+const swaggerOptions = {definition:{
+    openapi: '3.0.0',
+     info: {title: 'API Consejos',
+     version: '1.0.0',      
+    },
+    servers:[{url: "http://localhost:8082"}],  
+    },
+    apis: [`${path.join(__dirname,"./final.js")}`],
+  };
+
+
+app.use(express.json())
+app.use(express.text())
+app.use(cors({ origin:"http://localhost"}))
+
+app.use('/MostrarConsejos',ruta_consejo.router);
+app.use('/MostrarConsejos/',ruta_consejo.router);
+app.use('/AgregarConsejos',ruta_consejo.router);
+app.use('/actualizarConsejo/',ruta_consejo.router);
+app.use('/borrarConsejo/',ruta_consejo.router);
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerDocs));
+
+
+app.listen(8082, ()=>{console.log('Servidor corriendo express')})
---
+++ CRUD API REST/router_doc/test/test.js
@@ -0,0 +1,30 @@
+let   chai     = require('chai');
+let   chaiHttp = require('chai-http');
+const expect   = require('chai').expect;
+chai.use(chaiHttp);const url= 'http://localhost:8082';
+//Encapsular en test dentro de la funcion describe() Y describirmos el test
+
+describe('Inserte un consejo: ',()=>{     
+    it('Escribir consejos', (done) => {       
+        chai.request(url)   
+        .post('/AgregarConsejos/post')
+        .send({ consejo:"La curva de una sonrisa es capaz de enderezar muchas cosas." })
+        .end( function(err,res){
+            expect(res).to.have.status(200);        
+             done();
+            }); 
+    });
+});
+
+describe('Obtiene todos los consejos',()=>{   
+    it('Debe obtener todas los consejos.', (done) => {
+        chai.request(url)     
+        .get('/MostrarConsejos')
+        .send()
+        .end( function(err,res){
+            expect(res).to.have.status(200);   
+            expect(res).to.be.json;              
+            done();
+        });
+    });
+});
