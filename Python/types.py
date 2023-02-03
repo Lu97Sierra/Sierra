457c90d85fdca7b02fde71cbf1b20462311d680b
Luis Gustavo <70583538+Lu97Sierra@users.noreply.github.com>
2023-02-03 10:10:07 -0600

    origin

--- Python/types.py
+++ Python/types.py
@@ -86,3 +86,82 @@
 # print(saludo[::-1])
 # print(saludo)
 # print(''.join[reversed(saludo)])
+
+
+# tupla = (1,2,3)
+# print(tupla)
+# tupla = 1,2,3
+# print(type(tupla))
+# print(tupla)
+# #tupla[0]=5                     Marcaria error ya que las tuplas son inmutables
+# tupla = 1,2,('a','b'),3
+# print(tupla)
+# print(tupla[2][0])
+# tupla = 1,2,3
+# x,y,z = tupla
+# print(x,y,z)
+# lista=[1,2,3]
+# tupla = tuple(lista)
+
+# for t in tupla:
+#     print(t)
+
+# tupla = (2,2,2,2,2,2,2,2,3,6,5)
+
+# print(tupla.count(2))
+# print(tupla.index(3))
+# print(tupla.index(5,5))
+
+# lista = [1,2,3,4,5]
+# lista = list("1234")
+# print(lista)
+# lista = [1,"Hola",3.67,[1,2,3]]
+# print(lista)
+# print(lista[0])
+# lista[3][2]=100
+# print(lista[3][2])
+
+
+# l = [1,2,3,4,5,6,7,8,9]
+# l2 = l[0:4]
+# l3=[0,0,0]
+# l[0:3] = l3
+# l+=l3
+# print(l)
+
+# x,y,z = l3
+# print(x,y,z)
+lista=[1,2,3,4]
+for elemento in lista:
+    print(elemento)
+
+for index,l in enumerate(lista):
+    print(index,l)
+
+lista2 = ["c","z","a"]
+
+for l1,l2 in zip(lista,lista2):
+    print(l1,l2)
+
+for i in range(0,len(lista)):
+    print(lista[i])
+
+l = [2,3]
+l.append(3)
+print(l)
+l.extend([1,2,3,4])
+print(l)
+l.insert(0,100)
+print(l)
+l.remove(100)
+print(l)
+l.pop()
+print(l)
+
+l.reverse()
+print(l)
+
+l.sort()
+print(l)
+l.sort(reverse=True)
+print(l)
