11cb793d1b5b9e53f92cba041d7b1575b17cc3e0
Luis Gustavo <70583538+Lu97Sierra@users.noreply.github.com>
2023-02-28 14:57:26 -0600

    origin

--- Python/Conexion a oracle.py
+++ Python/Conexion a oracle.py
@@ -1,7 +1,7 @@
 import cx_Oracle
 
 # Conexión a la base de datos Oracle
-dsn_tns = cx_Oracle.makedsn('localhost', '1521', 'XE') # Reemplazar "localhost" con la dirección IP del servidor de Oracle
+dsn_tns = cx_Oracle.makedsn('localhost', '1521', 'XE') 
 conn = cx_Oracle.connect(user='SYSTEM', password='867163789', dsn=dsn_tns)
 
 # Ejecutar una consulta
