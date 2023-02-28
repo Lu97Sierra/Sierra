47db69be358bb118038841836ac4df571e37d591
Luis Gustavo <70583538+Lu97Sierra@users.noreply.github.com>
2023-02-28 14:48:46 -0600

    origin

---
+++ Python/Conexion a oracle.py
@@ -0,0 +1,17 @@
+import cx_Oracle
+
+# Conexión a la base de datos Oracle
+dsn_tns = cx_Oracle.makedsn('localhost', '1521', 'XE') # Reemplazar "localhost" con la dirección IP del servidor de Oracle
+conn = cx_Oracle.connect(user='SYSTEM', password='867163789', dsn=dsn_tns)
+
+# Ejecutar una consulta
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM usuarios")
+rows = cursor.fetchall()
+
+# Imprimir los resultados
+for row in rows:
+    print(row)
+
+# Cerrar la conexión
+conn.close()
